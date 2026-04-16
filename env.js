696951d75724cef9f5995596396e4a228278dd9a
Ugochukwu Ibecheozor <mbonuojike@Ugochukwus-Mac-mini.local>
2026-04-15 19:42:03 -0700

    chore: deployed env file

---
+++ env.js
@@ -0,0 +1,5 @@
+window.__ENV__ = {
+  SUBMIT_URL: "https://formspree.io/f/maqanzgv",
+  POSTHOG_KEY: "phc_yAFjbSzCk5ZK7xfmZBh6bgUcePkDWFfYRnxueVKjryAN",
+  POSTHOG_HOST: "https://us.i.posthog.com",
+};
