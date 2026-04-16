23574886d6c45e8ad3fb836763b78d13e387d473
Ugochukwu Ibecheozor <mbonuojike@Ugochukwus-Mac-mini.local>
2026-04-15 20:31:26 -0700

    chore: added stripe config

--- env.js
+++ env.js
@@ -2,4 +2,5 @@ window.__ENV__ = {
   SUBMIT_URL: "https://formspree.io/f/maqanzgv",
   POSTHOG_KEY: "phc_yAFjbSzCk5ZK7xfmZBh6bgUcePkDWFfYRnxueVKjryAN",
   POSTHOG_HOST: "https://us.i.posthog.com",
+  STRIPE_PAYMENT_LINK: "https://buy.stripe.com/test_28E3cueMqdIO1pN96d9ws00",
 };
